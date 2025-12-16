9961e2234574cb3ef5dcad4f77052277c4fa2c13
ajin <aj5210@naver.com>
2025-12-16 15:49:26 +0900

    chore: axios 설치

---
+++ apps/frontend/src/api/client.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 인터셉터 (필요시)
+apiClient.interceptors.request.use(
+  (config) => {
+    // 요청 전 처리
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 (필요시)
+apiClient.interceptors.response.use(
+  (response) => {
+    // 응답 후 처리
+    return response;
+  },
+  (error) => {
+    // 에러 처리
+    return Promise.reject(error);
+  }
+);
