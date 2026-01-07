754c1f8469c6108ab1544a032363267f63070fb8
withonewith <sky30a@korea.ac.kr>
2026-01-07 14:54:30 +0900

    fix: global prefix 위치를 최상단으로 조정

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('The API description')
@@ -12,7 +14,6 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
