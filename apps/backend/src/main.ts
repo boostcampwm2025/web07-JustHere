300ec2c69bc0d20d0cb8fc2f43878a3f1becb3db
ryugeon <sksmsfbrjs@naver.com>
2026-01-13 14:07:14 +0900

    feat: 생성해준 filter와 interceptor 적용

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { GlobalExceptionsFilter } from '@/lib/logger/global-exception.filter'
+import { LoggingInterceptor } from '@/lib/logger/logging.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalInterceptors(new LoggingInterceptor())
+  app.useGlobalFilters(new GlobalExceptionsFilter())
 
   app.setGlobalPrefix('api')
 
