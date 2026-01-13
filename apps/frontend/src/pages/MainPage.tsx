34cd4d4a14227f1703f3b3369192d16c28913825
ajin <aj5210@naver.com>
2026-01-13 23:16:57 +0900

    feat: 메인 페이지 렌더링 시 room 참가, 언마운트 시 room 퇴장

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
 import Header from '@/components/common/Header'
 import WhiteboardSection from '@/components/main/WhiteboardSection'
 import LocationListSection from '@/components/main/LocationListSection'
+import { useRoomSocket } from '@/hooks/useRoomSocket'
+import { MOCK_ROOM_ID, MOCK_USER } from '@/mocks'
 
 function MainPage() {
+  const { joinRoom, leaveRoom } = useRoomSocket()
+
+  useEffect(() => {
+    joinRoom(MOCK_ROOM_ID, MOCK_USER)
+    return () => leaveRoom()
+  }, [leaveRoom, joinRoom])
+
   return (
     <div className="flex flex-col h-screen bg-gray-bg">
       <Header />
