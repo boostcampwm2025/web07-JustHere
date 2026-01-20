21ba5d0adbe4956c26de5cc177c2bf9e7dee9d52
ajin <aj5210@naver.com>
2026-01-20 16:43:29 +0900

    feat: root에 토스트 provider, 컴포넌트 추가

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '@/index.css'
 import App from '@/App'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastProvider } from '@/contexts/ToastProvider'
+import { ToastContainer } from '@/components/common/ToastContainer'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +22,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <ToastProvider>
+          <App />
+          <ToastContainer />
+        </ToastProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
