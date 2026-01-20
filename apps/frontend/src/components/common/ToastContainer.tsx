7c9ca4656c91e55a286222f935b33a4b2092ea08
ajin <aj5210@naver.com>
2026-01-20 20:59:15 +0900

    design: 토스트 컴포넌트 하단에서 상단으로 수정
    - 하단의 캔버스/지도 탭과 UI가 겹쳐 캔버스 상단으로 변경

--- apps/frontend/src/components/common/ToastContainer.tsx
+++ apps/frontend/src/components/common/ToastContainer.tsx
@@ -15,7 +15,7 @@ export function ToastContainer() {
   if (toasts.length === 0) return null
 
   return (
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-80 flex flex-col gap-2">
+    <div className="fixed top-32 left-1/2 -translate-x-1/2 z-80 flex flex-col gap-2">
       {toasts.map(toast => (
         <div
           key={toast.id}
