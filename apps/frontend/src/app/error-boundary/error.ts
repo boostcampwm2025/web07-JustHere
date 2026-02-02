97fdc9cca64582f0c7230df00464e0d944ee184e
ajin <aj5210@naver.com>
2026-02-02 22:09:59 +0900

    feat: 클라이언트 에러 타입 추가

--- apps/frontend/src/app/error-boundary/error.ts
+++ apps/frontend/src/app/error-boundary/error.ts
@@ -1,4 +1,5 @@
 import type { SocketErrorType } from '@/shared/types'
+import { ERROR_TYPE } from './error-type'
 
 /**
  * 클라이언트 소켓 에러 타입 클래스
@@ -8,7 +9,7 @@ class SocketError extends Error {
 
   constructor(errorType: SocketErrorType, message: string) {
     super(message)
-    this.name = 'SocketError'
+    this.name = 'SOCKET_ERROR'
     this.errorType = errorType
   }
 }
@@ -19,7 +20,7 @@ class SocketError extends Error {
 export class RoomNotFoundError extends SocketError {
   constructor(message: string = '존재하지 않거나 삭제된 방입니다.') {
     super('NOT_FOUND', message)
-    this.name = 'RoomNotFoundError'
+    this.name = ERROR_TYPE.ROOM_NOT_FOUND
   }
 }
 
@@ -29,6 +30,16 @@ export class RoomNotFoundError extends SocketError {
 export class ResultNotFoundError extends Error {
   constructor(message: string = '투표 결과가 없습니다.') {
     super(message)
-    this.name = 'ResultNotFoundError'
+    this.name = ERROR_TYPE.RESULT_NOT_FOUND
+  }
+}
+
+/**
+ * 결과를 불러오는데 실패했을 때 발생하는 에러
+ */
+export class ResultLoadFailedError extends Error {
+  constructor(message: string = '결과를 불러오는데 실패했습니다.') {
+    super(message)
+    this.name = ERROR_TYPE.RESULT_LOAD_FAILED
   }
 }
--- apps/frontend/src/app/error-boundary/index.ts
+++ apps/frontend/src/app/error-boundary/index.ts
@@ -1,2 +1,3 @@
 export { RoomErrorBoundary } from './RoomErrorBoundary'
 export { RoomNotFoundError, ResultNotFoundError } from './error'
+export { ERROR_TYPE, type ErrorType } from './error-type'
